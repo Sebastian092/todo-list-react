78ef083b6cd44441fca816bb19da4ff5fed99f8f
Sebastian Szaładyga <sebastian.szaladyga@gmail.com>
2023-07-09 16:56:38 +0200

    added fokus on input

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,8 +1,13 @@
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 import { Button, FormSection, Input } from "./styled"
 
 const Form = ({ addNewTask }) => {
   const [newTaskContent, setNewTaskContent] = useState("");
+  const inputRef = useRef(null);
+
+  const fokusInput = () => {
+    inputRef.current.focus();
+  }
 
   const onFormSubmit = (event) => {
     event.preventDefault();
@@ -20,13 +25,18 @@ const Form = ({ addNewTask }) => {
   return (
     <FormSection onSubmit={onFormSubmit}>
       <Input
+      ref={inputRef}
         id="form"
         value={newTaskContent}
         type="text"
         placeholder="What is there to do?"
         onChange={({ target }) => setNewTaskContent(target.value)}
       />
-      <Button>Add a task</Button>
+      <Button
+      onClick={fokusInput}
+      >
+        Add a task
+      </Button>
     </FormSection>
   );
 };
