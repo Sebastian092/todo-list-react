0b1533c843036fac93fabbc5204a982a68ed1ece
Sebastian Szaładyga <sebastian.szaladyga@gmail.com>
2024-02-11 15:58:15 +0100

    consmetic change in styled.js - button

--- src/common/Header/styled.js
+++ src/common/Header/styled.js
@@ -2,7 +2,6 @@ import styled from "styled-components";
 
 export const MainHead = styled.header`
     padding: 0 20px;
-    border-bottom: 2px solid rgb(229, 227, 227);
     display: grid;
     grid-template-columns: auto auto;
     align-content: space-around;
