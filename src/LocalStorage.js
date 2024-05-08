8666d68d92c2fd875dbc6021e1f61c3a49c01487
Sebastian Szaładyga <sebastian.szaladyga@gmail.com>
2024-05-08 19:36:52 +0200

    local storage update

--- src/LocalStorage.js
+++
@@ -1,7 +0,0 @@
-const localStorageKey = "tasks";
-
-export const setLocalStorage = tasks =>
-    localStorage.setItem(localStorageKey, JSON.stringify(tasks));
-
-export const getLocalStorage = () =>
-    JSON.parse(localStorage.getItem(localStorageKey)) || [];
